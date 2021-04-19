ca8b64f4748de8c4976966cfd57b48a6966dc54d
jmd <jordan.michael.dickey@gmail.com>
2021-04-19 12:30:12 -0600

    Adding broswer router and setting up routes to the components in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import StoreFront from './containers/StoreFront'
+import {Switch, Route} from 'react-router-dom'
+import Cart from './components/Cart'
+import Default from './components/Default'
+import Details from './components/Details'
+import Navbar from './components/Navbar'
+import Product from './components/Product'
+import ProductList from './components/ProductList'
 class App extends React.Component{
 
   render(){
     return (
       <React.Fragment>
-        Hello
+        <Navbar />
+        <Switch>
+            <Route exact path="/" component={StoreFront} />
+            <Route path="/details" component={Details} />
+            <Route path="/cart" component={Cart} />
+            <Route component={Default} />
+        </Switch>
       </React.Fragment>
     );
   };
---
+++ src/containers/StoreFront.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class storefront extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Store Front</h3>
+               
+               
+            </div>
+        )
+    }
+}
