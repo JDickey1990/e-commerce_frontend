ab9b76e9a7070d6c1bddbec93bb676be5dba945b
jmd <jordan.michael.dickey@gmail.com>
2021-04-19 11:29:41 -0600

    importing bootstrap and App.css file for styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component{
 
   render(){
     return (
-      <div className="App">
-        App
-      </div>
+      <React.Fragment>
+        Hello
+      </React.Fragment>
     );
   };
  
