bba6544937bd8a2bf89c9a800643a252b4cdacf4
jmd <jordan.michael.dickey@gmail.com>
2021-04-19 19:12:40 -0600

    created a title component and rendered it in the ProductList component which is now imported and rendered from StoreFront

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import Product from './Product.js'
+import Title from './Title';
+
 export default class ProductList extends Component {
+
+    state={
+        products: []
+    }
+
     render() {
         return (
-            <div>
-                <h3>Hello from Product List</h3>
-                <Product />
+            <div className="py-5">
+               <div className="container"> 
+               <Title name={"our"} title={"products"} />
+                    <div className="row">
+
+                    </div>
+               </div>
             </div>
         )
     }
--- src/containers/StoreFront.js
+++ src/containers/StoreFront.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import ProductList from '../components/ProductList'
 
 export default class storefront extends Component {
     render() {
         return (
             <div>
                 <h3>Hello from Store Front</h3>
-               
+               <ProductList />
                
             </div>
         )
