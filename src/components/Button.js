4df84512972bd393f5149b4ad4cfc90cb7f30f3b
jmd <jordan.michael.dickey@gmail.com>
2021-04-20 15:29:05 -0600

    added a cart prop to write a ternary operator in the ButtonContainer so I could display the button in 2 different colors

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,14 +5,15 @@ text-transform: capitalize;
 font-size: 1.4rem;
 background: transparent;
 border: 0.5rem soid;
-// color:var(--lightBlue);
+border-color: ${props => props.cart ? "var(--mainYellow)": "var(--lightGrey)" };
+color: ${props => props.cart ? "var(--mainYellow)": "var(--lightGrey)" };
 border-radius: 0.5rem;
 padding: 0.2rem 0.5rem;
 cursor:pointer;
 margin:0.2rem 0.5rem 0.2rem 0;
 transition:all 2s ease-in-out;
 &:hover{
-    background:var(--lightGrey);
+    background:${props => props.cart ? "var(--mainYellow)": "var(--lightGrey)" };
     color:var(--mainGrey);
 }
 &:focus{
