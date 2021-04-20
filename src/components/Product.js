b7bd365c79fd1087d9e85b922158f68277aef5ed
jmd <jordan.michael.dickey@gmail.com>
2021-04-20 15:41:20 -0600

    Added an onClick function for the image of the product component to dispatch an action and update the state of the redux store for rendering product details

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,105 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+
+
 
 export default class Product extends Component {
+
+    handleDetailOnClick = () => {
+         this.props.findProduct(this.props.product.id)
+    }
+
+
     render() {
+        const {id, model, img, price, inCart} = this.props.product;
         return (
-            <div>
-                <h3>Hello from Product</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={() => this.handleDetailOnClick()}>
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top"/>
+                        </Link>
+                        <button 
+                            className="cart-btn" 
+                            disabled={inCart ? true:false} 
+                            onClick={()=> {console.log("write a function to add to the cart")
+                            }}
+                        >
+                            {inCart ? (
+                                <p className="text-capitalize mb-0" disabled> 
+                                    {" "}
+                                    In Cart
+                                </p>
+                                ) : (
+                                <i className="fas fa-cart-plus"/>
+                            )}
+                        </button>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                            <p className="align-self-center mb-0">
+                                {model}
+                            </p>
+                            <h5 className="text-grey font-italic mb-0">
+                                <span className="mr-1">$</span>
+                                {price}
+                            </h5>
+
+                    </div>
+                </div>
+            </ProductWrapper>
         )
     }
 }
+
+const ProductWrapper = styled.div`
+.card{
+    border-color: transparent;
+    transition: all 1s linear;
+}
+.card-footer{
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+}
+&:hover{
+    .card{
+        border:0.04rem solid rgba(0,0,0,0.2);
+        box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .card-footer{
+        background: rgba(247, 247, 247);
+    }
+}
+.img-container{
+    position: relative;
+    overflow: hidden;
+}
+.card-img-top{
+    transition: all 1s linear;
+}
+.img-container: hover .card-img-top{
+    transform: scale(1.2);
+}
+.cart-btn{
+    position: absolute;
+    bottom: 0;
+    right: 0:
+    padding: 0.2rem 0.4rem;
+    background:var(--lightGrey);
+    border: none;
+    color: var(--mainWhite);
+    font-size:1.4rem;
+    border-radius: 0.5 0 0 0;
+    transform: translate(100%, 100%);
+}
+.img-container:hover .cart-btn{
+    transform: translate(0,0);
+    transition: all 1s linear;
+}
+.cart-btn:hover{
+    color:var(--mainGrey);
+    cursor: pointer;
+}
+`;
