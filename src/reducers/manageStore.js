46eb7a8cab0a4f35f2a4cd378bc7c479fd75b565
jmd <jordan.michael.dickey@gmail.com>
2021-04-20 15:45:11 -0600

    Wrote an action in the manage store reducer to handle finding a product and update the product key in store to reflect the same value

--- src/reducers/manageStore.js
+++ src/reducers/manageStore.js
@@ -1,30 +1,26 @@
+import {storeProducts} from '../data';
+import {detailProduct} from '../data';
+
+// componentDidMount() {
+//     this.setProducts()
+// };
+// setProducts = () =>{
+//     let temp= [];
+//     storeProducts.forEach(item => {
+//         const singleItem = {...item};
+//         temp=[...temp, singleItem];
+//         return temp
+//     });
+// };
 
 export default function manageStore(state= 
-    { products: [], shoppingCart: [] }, action) {
+    { products: storeProducts, Product: detailProduct ,shoppingCart: [] }, action) {
         switch (action.type) {
-            // case 'ADD_RESTAURANT':
-            //   const restaurant = {
-            //     id: cuid(),
-            //     text: action.text
-            //   }
-            //   return { ...state, restaurants: [...state.restaurants, restaurant] }
-              
-            // case 'DELETE_RESTAURANT':
-            //   const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
-            //   return { ...state, restaurants };
+ 
+            case 'FIND_PRODUCT':
+                const selectedProduct = state.products.find(product => product.id === action.id);
+                return { ...state, product: selectedProduct}
 
-            // case 'ADD_REVIEW':
-            // const review = {
-            //     id: cuid(),
-            //     text: action.review.text,
-            //     restaurantId: action.review.restaurantId
-            // }
-            // return { ...state, reviews: [...state.reviews, review] }
-            
-            // case 'DELETE_REVIEW':
-            //     const reviews = state.reviews.filter(review => review.id !== action.id);
-            //     return {...state, reviews }
-          
             default:
               return state;
         }  
