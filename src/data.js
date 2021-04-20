33e8033286cb31b51ff5f4a08f1e4a64ec623ab4
jmd <jordan.michael.dickey@gmail.com>
2021-04-20 15:43:19 -0600

    rewrote the data file to include all of the watches and attributes needed

--- src/data.js
+++ src/data.js
@@ -1,108 +1,117 @@
 export const storeProducts = [
     {
       id: 1,
-      title: "Google Pixel - Black",
-      img: "img/product-1.png",
-      price: 10,
-      company: "GOOGLE",
-      info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      model: "5374P-001 - Platinum",
+      img: "img/Product-1_5374P_001.jpg",
+      price: 55000 ,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
+      info: "The Grand Complication with a minute repeater and a perpetual calendar sports a new face and a platinum case with gentle cambers and rounds emphasized by the concave bezel and the satin-finished, recessed flanks. The black dial in genuine enamel carries applied Breguet numerals and recessed counters.",
       inCart: false,
       count: 0,
       total: 0
     },
     {
       id: 2,
-      title: "Samsung S7",
-      img: "img/product-2.png",
-      price: 16,
-      company: "SAMSUNG",
+      model: "5208R-001 - Rose Gold",
+      img: "img/Product-2_5208R_001.jpg",
+      price: 63000,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
       info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "Patek Philippe has reinterpreted its Reference 5208 Grand Complication by housing it for the first time in a rose-gold case, paired with an ebony black sunburst dial. The 719-part movement, combining a minute repeater, a monopusher chronograph and an instantaneous perpetual calendar with aperture displays, is one of the rare ultra-complicated calibers to be self-winding. It can be admired through a sapphire-crystal case back.",
       inCart: false,
       count: 0,
       total: 0
     },
     {
       id: 3,
-      title: "HTC 10 - Black",
-      img: "img/product-3.png",
-      price: 8,
-      company: "htc",
+      model: "5270J-001 - Yellow Gold",
+      img: "img/Product-3_5270J_001.jpg",
+      price: 176240,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
       info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "As the heir of a grand Patek Philippe classic, the Ref. 5270 Chronograph with a perpetual calendar is now available in yellow gold for the first time – an elegant outfit for this timeless watch with a concave bezel and fluted lugs. The silvery opaline dial is superbly legible. The caliber CH 29-535 PS Q movement combines traditional design with six patented innovations and an extremely slender calendar mechanism.",
       inCart: false,
       count: 0,
       total: 0
     },
     {
       id: 4,
-      title: "HTC 10 - White",
-      img: "img/product-4.png",
-      price: 18,
-      company: "htc",
+      model: "5270P-001 - Platinum",
+      img: "img/Product-4_5270P_001.jpg",
+      price: 205810,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
       info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "Heir to a great Patek Philippe classic launched in 1941, the Reference 5270 perpetual calendar chronograph is now available in platinum for the first time. Its golden opaline dial, displaying the day and the month in a double aperture at 12 o’clock and an analog date at 6 o’clock, combines a hint of vintage elegance with great legibility. The chronograph movement, with its classic architecture – horizontal clutch, column wheel and manual winding – features six patented innovations.",
       inCart: false,
       count: 0,
       total: 0
     },
     {
       id: 5,
-      title: "HTC Desire 626s",
-      img: "img/product-5.png",
-      price: 24,
-      company: "htc",
+      model: "5271P-001 - Platinum",
+      img: "img/Product-5_5271P_001.jpg",
+      price: 289790,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
       info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "The chronograph has always featured prominently in the vast range of complications in which Patek Philippe has demonstrated its virtuosity. For over 175 years, the manufacture has created a wealth of different models, many of which count among the timepieces most admired and sought after by connoisseurs and collectors.",
       inCart: false,
       count: 0,
       total: 0
     },
     {
       id: 6,
-      title: "Vintage Iphone",
-      img: "img/product-6.png",
-      price: 17,
-      company: "apple",
+      model: "5303R-001 - White Gold",
+      img: "img/Product-6_5303R_001.jpg",
+      price: 423500,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
       info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "This new grand complication was created explicitly for connoisseurs of striking watches. It stands out with its open architecture that allows the minute repeater mechanism including the hammers and gongs to be admired in action while the watch is being worn on the wrist. The aesthetic appeal of the manually wound caliber R TO 27 PS was subtly reworked to highlight its full beauty. The rose-gold case features white-gold intarsias engraved with foliage motifs.",
       inCart: false,
       count: 0,
       total: 0
     },
     {
       id: 7,
-      title: "Iphone 7",
-      img: "img/product-7.png",
-      price: 30,
-      company: "apple",
+      model: "6102P-001 - Platinum",
+      img: "img/Product-7_6102P_001.jpg",
+      price: 334730,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
       info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "Highlighting Patek Philippe’s great tradition of astronomical watches, the Celestial devotes its dial to a rotating chart of the heavenly bodies. At any time, its owner may admire the exact configuration of the nocturnal sky in the northern hemisphere, with the apparent movement of the stars and the phases and orbit of the moon. Two skeletonized hands point to the hours and minutes of mean solar time. An Ellipse, deposited on the underside of the sapphire glass, frames the portion of the sky visible from Geneva and all other cities located at the same latitude.",
       inCart: false,
       count: 0,
       total: 0
     },
     {
       id: 8,
-      title: "Smashed Iphone",
-      img: "img/product-8.png",
-      price: 2,
-      company: "apple",
+      model: "5316P-001 - Platinum",
+      img: "img/Product-8_5316P_001.jpg",
+      price: 92260,
+      company: "Patek Philippe",
+      collection: "Grand Complications",
       info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        "Patek Philippe has redesigned its famous Grand Complication with a minute repeater, a tourbillon, and a perpetual calendar with a retrograde date. It sports a new platinum case with a two-tier bezel and lugs as well as a black enamel dial. The retrograde date is indicated by an unusual hand with a diamond-shaped tip.",
       inCart: false,
       count: 0,
       total: 0
     }
+
   ];
   
   export const detailProduct = {
     id: 1,
-    title: "Google Pixel - Black",
-    img: "img/product-1.png",
+    model: "Google Pixel - Black",
+    img: "img/Product-8_5316P_001.jpg",
     price: 10,
-    company: "google",
+    company: "Patek Philippe",
+    collection: "Grand Complications",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
