95d5b9f1c0287ec0e5a53b750b35cdee848c0469
jmd <jordan.michael.dickey@gmail.com>
2021-04-21 14:44:06 -0600

    Adding the case's of ADD_TO_CART, OPEN_MODAL and CLOSE_MODAL

--- src/reducers/manageStore.js
+++ src/reducers/manageStore.js
@@ -14,14 +14,29 @@ import {detailProduct} from '../data';
 // };
 
 export default function manageStore(state= 
-    { products: storeProducts, Product: detailProduct ,shoppingCart: [] }, action) {
+    { products: storeProducts, product: detailProduct, modalOpen: false, modalProduct: detailProduct ,shoppingCart: [] }, action) {
         switch (action.type) {
- 
+            case 'ADD_TO_CART':
+                const tempProducts = [...state.products];
+                const index = tempProducts.indexOf(action.product);
+                const product = tempProducts[index];
+                product.inCart = true;
+                product.count = 1;
+                const price = product.price;
+                product.total= price;
+                return {products: tempProducts, shoppingCart: [...state.shoppingCart, product ]}
+
+            case 'OPEN_MODAL':
+                return {...state, modalProduct: action.product, modalOpen: true, product: action.product}
+
+            case 'CLOSE_MODAL':
+                 return {...state, modalOpen: false}
+
             case 'FIND_PRODUCT':
                 const selectedProduct = state.products.find(product => product.id === action.id);
                 return { ...state, product: selectedProduct}
 
             default:
-              return state;
+                return state;
         }  
     }
