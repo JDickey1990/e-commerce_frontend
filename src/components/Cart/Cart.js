a54cdeb69f44875be893e8ec8b0d7b1e0ec495df
jmd <jordan.michael.dickey@gmail.com>
2021-04-27 16:34:33 -0600

    recreating the Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 import Title from '../Title'
 import CartColumns from './CartColumns'
+import CartList from './CartList'
+import CartTotals from './CartTotals'
+import EmptyCart from './EmptyCart'
+import { connect } from 'react-redux'
 
 
-export default class Cart extends Component {
+class Cart extends Component {
+
+    cart = () => {
+        if(this.props.shoppingCart.length > 0){
+            return <div>
+                    <Title name="your" title="cart" />
+                    <CartColumns />
+                    <CartList items={this.props.shoppingCart}/>
+                    <CartTotals />
+                </div> 
+            }else{
+               return <div>
+                        <EmptyCart/>
+                     </div>
+        }
+    }
+    
     render() {
         return (
           <section>
-              <Title name="your" title="cart" />
-              <CartColumns />
+              {this.cart()}
           </section>
         )
     }
 }
+
+const mapStateToProps = ({shoppingCart}) => ({shoppingCart})
+
+export default connect(mapStateToProps)(Cart)
